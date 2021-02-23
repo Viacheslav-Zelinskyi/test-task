8ec12f6bf42cb51d23c810aa17118b66ec7ffcaf
Viacheslav Zelinskyi <viacheslavzelinskyi@gmail.com>
2021-02-23 11:54:06 +0100

    Added graphql-request and react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { ReactQueryDevtools } from 'react-query/devtools'
+import { useForm } from 'react-hook-form';
 
 function App() {
   return (
